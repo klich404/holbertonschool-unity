5295a9e551de24efc50e542fc252841c3549ecef
klich404 <klich4010@gmail.com>
2022-01-26 17:23:27 -0500

    timer implemented

--- 0x05-unity-assets_models_textures/0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x05-unity-assets_models_textures/0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -6,15 +6,16 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text TimerText;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float time = 0f;
+    int mins = 0;
+    float secs = 0.00f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        mins = (int) time / 60;
+        secs =  time % 60f;
+        TimerText.text = mins.ToString() + ":" + secs.ToString("00.00");
     }
 }
---
+++ 0x05-unity-assets_models_textures/0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+            other.gameObject.GetComponent<Timer>().enabled = true;
+    }
+}
