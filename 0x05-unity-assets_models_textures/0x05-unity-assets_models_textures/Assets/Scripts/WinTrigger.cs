ef3774b83f477b645dd88b13cd3f73358fc56d7f
klich404 <klich4010@gmail.com>
2022-01-28 17:39:30 -0500

    Assets implemented

---
+++ 0x05-unity-assets_models_textures/0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text timerText;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+            other.gameObject.GetComponent<Timer>().enabled = false;
+            timerText.fontSize = 60;
+            timerText.color = Color.green;
+    }
+}
