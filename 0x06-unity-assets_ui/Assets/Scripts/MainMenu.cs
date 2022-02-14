7b91f525dd4667894dca88d80b20cf9566bb987f
Carlos David Cruz Zuluaga <70548247+klich404@users.noreply.github.com>
2022-02-14 15:54:12 -0500

    menu UI design

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level)
+    {
+        if (level == 1)
+            
+    }
+}
